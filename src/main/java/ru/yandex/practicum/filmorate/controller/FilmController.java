b89d89b5a967fdff724dbf562bfd2eb15c277637
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-05-25 23:36:43 +0300

    feat: add logs

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
 
@@ -29,16 +31,19 @@ public class FilmController {
         filmValidationControl(film);
         film.setId(getNextFilmId());
         films.put(film.getId(), film);
+        log.info("Created film with id={}:\n{}", film.getId(), film);
         return film;
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody @Validated(Update.class) @NonNull Film film) {
         if (!films.containsKey(film.getId())) {
+            log.warn("Attempt to update film with unknown id={}:\n{}", film.getId(), film);
             throw new ValidationException("Фильма с таким ID не зарегистрировано");
         }
         filmValidationControl(film);
         films.put(film.getId(), film);
+        log.info("Updated film with id={}:\n{}", film.getId(), film);
         return film;
     }
 
@@ -58,12 +63,16 @@ public class FilmController {
 
     private void filmValidationControl(Film film) {
         if (film.getName().isBlank()) {
+            log.warn("Attempt to use film with empty name id={}:\n{}", film.getId(), film);
             throw new ValidationException("Название фильма не должно быть пустым");
         } else if (film.getDescription().length() > 200) {
+            log.warn("Attempt to use film with description length more than 200 id={}:\n{}", film.getId(), film);
             throw new ValidationException("Описание фильма не должно быть более 200 символов");
         } else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("Attempt to use film with release date early than 1895-12-28 id={}:\n{}", film.getId(), film);
             throw new ValidationException("Дата релиза должна быть не раньше 1895-12-28");
         } else if (film.getDuration().isNegative()) {
+            log.warn("Attempt to use film with release negative Duration id={}:\n{}", film.getId(), film);
             throw new ValidationException("Продолжительность фильма должна быть положительным числом");
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private final Map<Integer, User> users = new HashMap<>();
 
@@ -30,17 +32,20 @@ public class UserController {
         user.setId(getNextNextId());
         ifNameEmptyFillWithLogin(user);
         users.put(user.getId(), user);
+        log.info("Created user with id={}:\n{}", user.getId(), user);
         return user;
     }
 
     @PutMapping
     public User updateUser(@RequestBody @Validated(Update.class) @NonNull User user) {
         if (!users.containsKey(user.getId())) {
+            log.warn("Attempt to update user with unknown id={}:\n{}", user.getId(), user);
             throw new ValidationException("Пользователя с таким ID не зарегестрировано");
         }
         userValidationControl(user);
         ifNameEmptyFillWithLogin(user);
         users.put(user.getId(), user);
+        log.info("Updated user with id={}:\n{}", user.getId(), user);
         return user;
     }
 
@@ -66,14 +71,19 @@ public class UserController {
 
     private void userValidationControl(User user) {
         if (user.getEmail().isBlank()) {
+            log.warn("Attempt to use User with empty email id={}:\n{}", user.getId(), user);
             throw new ValidationException("E-mail не может быть пустым");
         } else if (!user.getEmail().matches("([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9_-]+)")) {
+            log.warn("Attempt to use User with incorrect email id={}:\n{}", user.getId(), user);
             throw new ValidationException("Указан некорректный e-mail");
         } else if (user.getLogin().isBlank()) {
+            log.warn("Attempt to use User with empty login id={}:\n{}", user.getId(), user);
             throw new ValidationException("Логин не может быть пустым");
         } else if (!user.getLogin().matches("^\\S+$")) {
+            log.warn("Attempt to use User login with spaces id={}:\n{}", user.getId(), user);
             throw new ValidationException("Логин не может содержать пробелы");
         } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Attempt to use User with birthday in future id={}:\n{}", user.getId(), user);
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
     }
