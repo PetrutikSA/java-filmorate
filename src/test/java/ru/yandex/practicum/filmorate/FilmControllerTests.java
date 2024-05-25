42e26246b296b31d51c9c96d69860cfb81df6df2
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-05-25 23:44:34 +0300

    test: realize FilmController tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
@@ -0,0 +1,151 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FilmControllerTests {
+
+    private FilmController filmController;
+    private Film film;
+    private Film updatedFilm;
+    private final String notCorrectName = " ";
+    private final String notCorrectDescription = "Пятеро друзей ( комик-группа «Шарло»), приезжают в город Бризуль. " +
+            "Здесь они хотят разыскать господина Огюста Куглова, который задолжал им деньги, а именно 20 миллионов. " +
+            "о Куглов, который за время «своего отсутствия», стал кандидатом Коломбани.";
+    private final LocalDate notCorrectReleaseDate = LocalDate.of(1800, 10, 15);
+    private final Duration notCorrectDuration = Duration.ofMinutes(-120);
+    private final String returnNotCorrectFilm = "Фильм в базе данных не равен соответствует отправленному";
+    private final String returnFilmsListNotCorrectSize = "Некорректный размер списка фильмов";
+    private final String emptyNameValidationViolation = "Фильм без имени должен приводить к ошибке";
+    private final String descriptionSizeValidationViolation = "Фильм с описанием более 200 символов должен приводить к ошибке";
+    private final String releaseDateBorderValidationViolation = "Фильм отрицательной продолжительностью должен приводить к ошибке";
+    private final String negativeDurationValidationViolation = "Фильм отрицательной продолжительностью должен приводить к ошибке";
+
+
+    @BeforeEach
+    void beforeEach() {
+        filmController = new FilmController();
+        film = new Film(null, "name1", "Description1",
+                LocalDate.of(2000, 10, 15), Duration.ofMinutes(120));
+        updatedFilm = new Film(1, "updatedName", "UpdatedDescription",
+                LocalDate.of(2010, 1, 1), Duration.ofMinutes(180));
+    }
+
+    @Test
+    void filmIsNullThrowException() {
+        assertThrows(NullPointerException.class, () -> filmController.addFilm(null),
+                "Пустой запрос должен приводить к ошибке");
+    }
+
+    @Test
+    void correctFilmCreated() {
+        filmController.addFilm(film);
+        List<Film> filmList = filmController.getAllFilms();
+        assertEquals(1, filmList.size(), returnFilmsListNotCorrectSize);
+        Film filmFromController = filmController.getAllFilms().get(0);
+        film.setId(1);
+        assertEquals(film, filmFromController, returnNotCorrectFilm);
+    }
+
+    @Test
+    void createEmptyNameFilmThrowsException() {
+        film.setName(notCorrectName);
+        assertThrows(ValidationException.class, () -> filmController.addFilm(film),
+                emptyNameValidationViolation);
+    }
+
+    @Test
+    void createFilmDescriptionLengthMore200ThrowsException() {
+        film.setDescription(notCorrectDescription);
+        assertThrows(ValidationException.class, () -> filmController.addFilm(film),
+                descriptionSizeValidationViolation);
+    }
+
+    @Test
+    void createFilmRealiseDateEarlierDateOfFirstFilmThrowsException() {
+        film.setReleaseDate(notCorrectReleaseDate);
+        assertThrows(ValidationException.class, () -> filmController.addFilm(film),
+                releaseDateBorderValidationViolation);
+    }
+
+    @Test
+    void createFilmDurationNegativeValueThrowsException() {
+        film.setDuration(notCorrectDuration);
+        assertThrows(ValidationException.class, () -> filmController.addFilm(film),
+                negativeDurationValidationViolation);
+    }
+
+    @Test
+    void updateFilmWithoutIdThrowsException() {
+        filmController.addFilm(film);
+        updatedFilm.setId(null);
+        assertThrows(ValidationException.class, () -> filmController.updateFilm(updatedFilm),
+                "Обновление фильма без ID должно приводить к ошибке");
+    }
+
+    @Test
+    void correctFilmUpdate() {
+        filmController.addFilm(film);
+        filmController.updateFilm(updatedFilm);
+        List<Film> filmList = filmController.getAllFilms();
+        assertEquals(1, filmList.size(), returnFilmsListNotCorrectSize);
+        Film filmFromController = filmController.getAllFilms().get(0);
+        assertEquals(updatedFilm, filmFromController, returnNotCorrectFilm);
+    }
+
+    @Test
+    void updateEmptyNameFilmThrowsException() {
+        filmController.addFilm(film);
+        updatedFilm.setName(notCorrectName);
+        assertThrows(ValidationException.class, () -> filmController.updateFilm(updatedFilm),
+                emptyNameValidationViolation);
+    }
+
+    @Test
+    void updateFilmDescriptionLengthMore200ThrowsException() {
+        filmController.addFilm(film);
+        updatedFilm.setDescription(notCorrectDescription);
+        assertThrows(ValidationException.class, () -> filmController.updateFilm(updatedFilm),
+                descriptionSizeValidationViolation);
+    }
+
+    @Test
+    void updateFilmRealiseDateEarlierDateOfFirstFilmThrowsException() {
+        filmController.addFilm(film);
+        updatedFilm.setReleaseDate(notCorrectReleaseDate);
+        assertThrows(ValidationException.class, () -> filmController.updateFilm(updatedFilm),
+                releaseDateBorderValidationViolation);
+    }
+
+    @Test
+    void updateFilmDurationNegativeValueThrowsException() {
+        filmController.addFilm(film);
+        updatedFilm.setDuration(notCorrectDuration);
+        assertThrows(ValidationException.class, () -> filmController.updateFilm(updatedFilm),
+                negativeDurationValidationViolation);
+    }
+
+    @Test
+    void correctReturnAllFilms() {
+        filmController.addFilm(film);
+        Film film2 = new Film(null, "name2", "description2",
+                LocalDate.of(2010, 1, 1), Duration.ofMinutes(180));
+        filmController.addFilm(film2);
+        List<Film> filmList = filmController.getAllFilms();
+        assertEquals(2, filmList.size(), returnFilmsListNotCorrectSize);
+        film.setId(1);
+        assertEquals(film, filmController.getAllFilms().get(0), returnNotCorrectFilm);
+        film2.setId(2);
+        assertEquals(film2, filmController.getAllFilms().get(1), returnNotCorrectFilm);
+    }
+}
