f8c5091d8cc8ca4303aa73784e54498bcfe4c576
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-07-09 23:13:21 +0300

    fix: add FilmCreateRequest annotations

--- src/main/java/ru/yandex/practicum/filmorate/dto/film/FilmCreateRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/film/FilmCreateRequest.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.dto.film;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +28,7 @@ public class FilmCreateRequest {
     @NotBlank(message = "Название фильма не должно быть пустым")
     private String name;
     @Size(max = 200, message = "Описание фильма не должно быть более 200 символов")
+    @NotBlank
     private String description;
     @After(value = "1895-12-28", message = "Дата релиза должна быть не раньше 1895-12-28")
     private LocalDate releaseDate;
@@ -35,6 +37,7 @@ public class FilmCreateRequest {
     @JsonDeserialize(using = JsonDurationDeserializer.class)
     private Duration duration;
     private LinkedHashSet<Genre> genres;
+    @NotNull
     private Rating mpa;
 
 }
