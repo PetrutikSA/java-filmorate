3a6ae90476d24562c53698f9ef1d955fbd7c0e2d
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-07-09 22:18:31 +0300

    fix: remove validation annotations from model classes

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,20 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.time.DurationMin;
-import ru.yandex.practicum.filmorate.model.adapter.JsonDurationDeserializer;
-import ru.yandex.practicum.filmorate.model.adapter.JsonDurationSerializer;
 import ru.yandex.practicum.filmorate.model.enums.Genre;
 import ru.yandex.practicum.filmorate.model.enums.Rating;
-import ru.yandex.practicum.filmorate.model.validator.annotation.After;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -26,19 +17,10 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Film {
-    @Min(value = 1)
     private Integer id;
-    @NotBlank(message = "Название фильма не должно быть пустым")
     private String name;
-    @Size(max = 200,
-            message = "Описание фильма не должно быть более 200 символов")
     private String description;
-    @After(value = "1895-12-28",
-            message = "Дата релиза должна быть не раньше 1895-12-28")
     private LocalDate releaseDate;
-    @DurationMin(message = "Продолжительность фильма должна быть положительным числом")
-    @JsonSerialize(using = JsonDurationSerializer.class)
-    @JsonDeserialize(using = JsonDurationDeserializer.class)
     private Duration duration;
     private Set<Integer> usersIdPostedLikes;
     private LinkedHashSet<Genre> genres;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,10 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,16 +14,10 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    @Min(value = 1, message = "ID долно быть положжительным числом")
     private Integer id;
-    @Email(message = "Введен некорректный e-mail")
-    @NotBlank(message = "E-mail не может быть пустым")
     private String email;
-    @NotBlank(message = "Логин не может быть пустым")
-    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
     private String name;
-    @Past(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
     private Map<Integer, FriendshipStatus> friendsId;
 }
